e387b63e61671b5f0005794149782f729c4c9364
Michael Waddell <35202179+mjwaddell1@users.noreply.github.com>
2024-05-26 10:16:41 -0400

    Update AntForage.py

--- AntForage/AntForage.py
+++ AntForage/AntForage.py
@@ -11,14 +11,16 @@ from random import randint
 #    Ants don't look for new food during return trip
 #    Latest found food path is used by rest of colony, previous paths deactivated
 #    Food path may not be the most direct route, just latest path found
+#    Food runs out after 100 ants visit and resets at new location. Food level\color fades with each visit.
 #    Multiple ants may be at same position, so single dot shown
-#    If ant hits obstacle on return path, ant is reset to nest
-#    In reality, ants can can smell food 100 miles away if not obstacles
+#    If ants hit obstacle on return path, ant is reset to nest
+#    In reality, ants can can smell food 100 miles away if no obstacles
 
 # Possible add-ons:
-#    Food runs out, create new food source
 #    Multiple food sources
+#    Multiple obstacles
 #    Predator attacks nest
+#    Staggered search
 
 class Point: # for nest and food
     def __init__(self, x, y):
@@ -38,8 +40,9 @@ ant_speed = 3 # max pixel movement per step
 ant_cnt = 50 # dot count
 food_smell_dist = 30 # ant can detect food
 tracer_ants = []  #[0,1,2] # highlight ants for tracking
-box = Point(300, 300)
-boxside = 50
+box = Point(300, 300) # obstacle
+box_side = 50 # height and width
+food_level = 100 # percent
 
 scr_width = 1000
 scr_height = 700
@@ -47,7 +50,7 @@ window = pygame.display.set_mode((scr_width, scr_height))
 pygame.display.set_caption("Ants")
 
 def InBox(x, y):
-    return box.x-1 <= x <= box.x+boxside+1 and box.y-1 <= y <= box.y+boxside+1
+    return box.x-1 <= x <= box.x+box_side+1 and box.y-1 <= y <= box.y+box_side+1
 
 def DistancePt(pt1, pt2):
     return (sqrt((pt1.x - pt2.x) ** 2 + (pt1.y - pt2.y) ** 2))
@@ -61,6 +64,17 @@ def Degrees(pt1, pt2): # pt1 relative to pt2
     if deg < 0:  # convert to full circle
         return math.pi + (math.pi + deg)  # 0 - 2PI
     return deg
+
+def MoveFood(): # to random position
+    while True:
+        x = randint(200, 800) # screen 0-999
+        y = randint(100, 600) # screen 0-699
+        if DistanceXY(nest, x, y) < 50: # too close to nest
+            continue
+        if InBox(x, y): # overlapping obstacle
+            continue
+        food.x, food.y = (x, y)
+        break
 
 class Ant:
     def __init__(self, x=0, y=0, index=0):
@@ -82,7 +96,7 @@ class Ant:
         pygame.draw.circle(window, color, (self.x + xd, self.y + yd), 2)
 
     def MoveStep(self, dist):
-        global food_trails
+        global food_trails, food_level
         if len(self.trail) == 1: # at nest (direction is 0 or 1)
             food_trail_idx = -1 # food trail index
             # find active food trail
@@ -115,6 +129,10 @@ class Ant:
                     self.direction = -1 # return to nest
                     self.trail_pos -= 1
                 else: # found food
+                    food_level -= 1
+                    if food_level <= 1:
+                        MoveFood() # respawn food at random spot
+                        food_level = 100
                     self.direction = -1 # return to nest
                     self.trail_pos -= 1
             else: # next step toward food
@@ -213,7 +231,7 @@ def CheckEvents(): # check if user clicked key/mouse
                 food.x, food.y = event.dict['pos']
             if event.dict['button'] == 3: # move box
                 old = (box.x, box.y) # may need to revert move
-                box.x, box.y = (event.dict['pos'][0]-boxside/2, event.dict['pos'][1]-boxside/2)
+                box.x, box.y = (event.dict['pos'][0] - box_side / 2, event.dict['pos'][1] - box_side / 2)
                 if InBox(nest.x, nest.y): # box on nest
                     box.x, box.y = old # revert move
                     continue
@@ -242,6 +260,10 @@ while True: # run until escape pressed
     # update screen with new positions
     window.fill((0, 0, 0)) # clear screen
     WriteText() # instruction text
+    # nest is white dot at screen center
+    pygame.draw.circle(window, (255,255,255), (nest.x, nest.y), 5) # circle center
+    # food color fades according to food level
+    pygame.draw.circle(window, (55, 55 + 2 * food_level, 55), (food.x, food.y), 5) # circle center
     for i in range(ant_cnt): # move dots
         if i in tracer_ants:
             ants[i].Draw((255,200,200)) # white, highlight dots for tracking
@@ -252,8 +274,6 @@ while True: # run until escape pressed
         # text_surface = my_font.render(str(dots[i].index), False, (200, 200, 200))
         # window.blit(text_surface, (dots[i].x, dots[i].y))
 
-    pygame.draw.circle(window, (255,255,255), (nest.x, nest.y), 5) # circle center
-    pygame.draw.circle(window, (55,255,55), (food.x, food.y), 5) # circle center
-    pygame.draw.rect(window, (150, 50, 50), (box.x, box.y, boxside, boxside)) # box
+    pygame.draw.rect(window, (150, 50, 50), (box.x, box.y, box_side, box_side)) # box
     pygame.display.update() # show updates
     time.sleep(.1) # slow movement
